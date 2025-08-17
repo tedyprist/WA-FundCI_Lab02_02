33e561c3d5381ec5dbe11009e1a8b0dc84cce870
Tedy Prist <tedy.prist@icomp.ufam.edu.br>
2025-08-17 16:58:32 -0400

    First Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect', 
+    },
+  },
+  rules: {
+    'react/prop-types': 'off', 
+  },
+};
